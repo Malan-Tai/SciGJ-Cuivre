9efeac6b847c37138831524083d6cd3edace47ad
Malan-Tai <malantai99@gmail.com>
2021-04-09 23:59:31 +0200

    Added Random

---
+++ Cuivre/Code/Utils/Dice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuivre.Code.Utils
+{
+    static class Dice
+    {
+        static Random rand = new Random();
+        public static int GetRandint(int min, int max)
+        // max is inclusive
+        {
+            return rand.Next(min, max + 1);
+        }
+    }
+}
