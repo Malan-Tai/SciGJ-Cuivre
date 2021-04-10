13a1a83b6eeddf548a9d93e0f5bf3c03631e1d6e
Malan-Tai <malantai99@gmail.com>
2021-04-10 09:39:03 +0200

    Added button mother class

---
+++ Cuivre/Code/Button.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Cuivre.Code
+{
+    abstract class Button
+    {
+        public abstract void Draw(SpriteBatch spriteBatch);
+
+        public abstract void Click();
+    }
+}
