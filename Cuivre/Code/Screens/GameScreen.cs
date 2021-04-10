f8f0b8ab1035e57fbbd9c0d6143f174e5ce9e092
Malan-Tai <malantai99@gmail.com>
2021-04-10 09:39:24 +0200

    Added GameScreen base

---
+++ Cuivre/Code/Screens/GameScreen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Cuivre.Code.Screens
+{
+    class GameScreen : Screen
+    {
+        private Timeline timeline;
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
